ee7bb1f9725259b79b2d60fa53eddcf57f0503b1
kendrickPC <kennethpchang@gmail.com>
2021-07-27 13:06:23 -0700

    Components built and UI rendered

---
+++ src/Components/SearchResults/SearchResults.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './SearchResults.css';
+
+export function SearchResults() {
+  return (
+    <div className="SearchResults">
+      <h2>Results</h2>
+      {/* <!-- Add a TrackList component --> */}
+    </div>
+  )
+}
+
+
+
---
+++ src/Components/TrackList/TrackList.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './TrackList.css';
+import { Track } from '../Track/Track';
+
+export function TrackList() {
+  return (
+    <div className="TrackList">
+        {/* <!-- You will add a map method that renders a set of Track components  --> */}
+        <Track />
+    </div>
+  )
+};
+

9addcc39568e64a8d7f0d40cfe67de02736281b0
kendrickPC <kennethpchang@gmail.com>
2021-07-27 12:12:26 -0700

    App.js and App.css components rendered

---
+++ src/Components/App/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <h1>Kenneth's <span className="highlight">SPOT</span>IFY</h1>
+      <div className="App">
+        {/* <!-- Add a SearchBar component --> */}
+        <div className="App-playlist">
+          {/* <!-- Add a SearchResults component --> */}
+          {/* <!-- Add a Playlist component --> */}
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;

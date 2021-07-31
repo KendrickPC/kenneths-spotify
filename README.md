728b77d2bab3200383e8ab4ca364b207cb9e4da0
kendrickPC <kennethpchang@gmail.com>
2021-07-31 12:45:19 -0500

    state of playlistName can be changed

--- README.md
+++ README.md
@@ -2,7 +2,4 @@
 
 ### How I Built This:
 
-Inside of the App constructor, set this.state to an object with a property called searchResults set to an array of objects, each containing name, artist, album, and id properties.
-
-##### Remove Tracks from Playlist
 
--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -11,6 +11,7 @@ class App extends React.Component {
     
     this.addTrack = this.addTrack.bind(this);
     this.removeTrack = this.removeTrack.bind(this);
+    this.updatePlaylistName = this.updatePlaylistName.bind(this);
 
     this.state = {
       searchResults: [
@@ -54,6 +55,10 @@ class App extends React.Component {
     this.setState( {playlistTracks: tracks} );
   }
 
+  updatePlaylistName(name) {
+    this.setState( {playlistName: name} )
+  }
+
   render () {
     return (
       <div>
@@ -72,6 +77,7 @@ class App extends React.Component {
             playlistName={this.state.playlistName}
             playlistTracks={this.state.playlistTracks}
             onRemove={this.removeTrack}
+            onNameChange={this.updatePlaylistName}
           />
         </div>
       </div>
